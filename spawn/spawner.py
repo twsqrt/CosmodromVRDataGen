5e0de29d864db503bc2f60baba1e4091d806f9bf
twsqrt <twsqrt@gmail.com>
2024-06-16 20:14:26 +0300

    Added targets

--- spawn/spawner.py
+++ spawn/spawner.py
@@ -1,35 +1,38 @@
 import spawn.config as conf
+import spawn.targets as targets
 import random
 
 
 __targets_buffer = []
 
 
-def update_targets(elapsed_time: float) -> None:
-    for i, position in enumerate(__targets_buffer):
-        x, y, z = position
-        z += elapsed_time * conf.TARGET_SPEED
-        if z > 2 * conf.TARGET_RADIUS:
-            del __targets_buffer[i] 
-        else:
-            __targets_buffer[i] = x, y, z
-         
+def update_targets_buffer(elapsed_time: float) -> None:
+    for i, target in enumerate(__targets_buffer):
+        traveled_distance = target.get_traveled_distance_at(elapsed_time)
+        if traveled_distance > 2 * conf.TARGET_RADIUS:
+            del __targets_buffer[i]
 
-def try_find_free_position():
+
+def try_find_free_position(spawn_time: float, trajectory: str):
     for _ in range(0, conf.NUMBER_OF_SPAWN_ATTEMPS):
         x = random.uniform(-1, 1) * conf.BOX_WIDTH / 2
         y = random.uniform(-1, 1) * conf.BOX_HEIGHT / 2
 
+        new_target = targets.create_target(spawn_time, (x, y), trajectory)
         overlaps = False
 
-        for other_x, other_y, other_z in __targets_buffer:
-            square_dist = (other_x - x) ** 2 + (other_y - y) ** 2 + other_z ** 2
-            if square_dist < (2 * conf.TARGET_RADIUS) ** 2:
+        for target in __targets_buffer:
+            if targets.has_overlaps_on_interval(
+                spawn_time,
+                spawn_time + conf.TRAJECTORY_PERIOD,
+                new_target,
+                target,
+            ):
                 overlaps = True
                 break
         
         if not overlaps:
-            return x, y
+            return (x, y, new_target)
 
     return None
 
@@ -49,8 +52,12 @@ def get_random_trajectory() -> str:
             return name
          
 
-def create_data_row(spawn_time: float, x_pos: float, y_pos: float) -> list:
-    trajectory = get_random_trajectory()
+def create_data_row(
+    spawn_time: float, 
+    x_pos: float, 
+    y_pos: float, 
+    trajectory: str
+) -> list:
     has_rotation = (random.uniform(0, 1) < conf.ADD_ROTATION_PROBABILITY)
 
     return [
@@ -77,12 +84,15 @@ def simulate(simulation_time: float, reset_targets_buffer: bool = False) -> list
         spawn_time = get_spawn_time()
         elapsed_time += spawn_time
         
-        update_targets(spawn_time)
-        position = try_find_free_position()
+        update_targets_buffer(elapsed_time)
+
+        trajectory = get_random_trajectory()
+        position = try_find_free_position(elapsed_time, trajectory)
 
         if position is not None:
-            x, y = position
-            row = create_data_row(elapsed_time, x, y)
+            x, y, target = position
+            __targets_buffer.append(target)
+            row = create_data_row(elapsed_time, x, y, trajectory)
             spawn_data.append(row)
     
     return spawn_data
---
+++ spawn/targets.py
@@ -0,0 +1,82 @@
+import spawn.config as conf
+import math
+
+
+class Target:
+    def __init__(self, spawn_time: float, start_pos_on_box: tuple, trajectory_function: callable) -> None:
+        self.spawn_time = spawn_time
+        self.trajectory_function = trajectory_function
+        self.start_pos_on_box = start_pos_on_box
+
+    
+    def get_position_at(self, time: float) -> tuple:
+        lifetime = time - self.spawn_time 
+
+        if lifetime < 0:
+            return None
+        
+        traj_x, traj_y = self.trajectory_function(lifetime)
+        start_x, start_y = self.start_pos_on_box
+
+        return (
+            traj_x * conf.TRAJECTORY_SCALE + start_x,
+            traj_y * conf.TRAJECTORY_SCALE + start_y, 
+            lifetime * conf.TARGET_SPEED
+        )
+    
+
+    def get_traveled_distance_at(self, time: float) -> float:
+        lifetime = time - self.spawn_time
+        return lifetime * conf.TARGET_SPEED if lifetime > 0 else 0
+   
+    
+
+
+def __zig_zig(t: float) -> float:
+    fraction = t % 1
+    return 4 * abs(fraction - 0.5) - 1
+
+
+def __spiral(t: float) -> float:
+    angle = t * 2 * math.pi
+    return (math.cos(angle), math.sin(angle))
+
+
+def create_target(spawn_time: float, start_pos_on_box: tuple, trajectory: str) -> Target:
+    trajectory_function = None
+
+    match trajectory:
+        case 'Linear':
+            trajectory_function = lambda _: (0, 0)
+        case 'HorizontalZigZag':
+            trajectory_function = lambda t: (__zig_zig(t), 0)
+        case 'VerticalZigZag':
+            trajectory_function = lambda t: (0, __zig_zig(t))
+        case 'Spiral':
+            trajectory_function = __spiral
+          
+    return Target(spawn_time, start_pos_on_box, trajectory_function)
+
+
+def is_overlaps(time: float, first: Target, second: Target) -> bool:
+    x1, y1, z1 = first.get_position_at(time)
+    x2, y2, z2 = second.get_position_at(time)
+
+    square_distance = (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
+    return (square_distance > (2 * conf.TARGET_RADIUS) ** 2)
+
+
+def has_overlaps_on_interval(start_time: float, 
+    end_time: float, 
+    first: Target, 
+    second: Target,
+    delta_time: float = 1 /30,
+) -> bool:
+    elapsed_time = start_time
+
+    while elapsed_time < end_time:
+        if is_overlaps(elapsed_time, first, second):
+            return True
+        elapsed_time += delta_time
+    
+    return False
